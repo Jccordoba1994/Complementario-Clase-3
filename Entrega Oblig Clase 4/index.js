ed165e43f6da8e90e49522128523d623b5e4ff0f
Juan Cruz Cordoba <juanccordoba18@gmail.com>
2022-06-23 02:23:28 -0300

    Entregable clase 4

--- Entrega Oblig Clase 4/index.js
+++ Entrega Oblig Clase 4/index.js
@@ -1,27 +1,58 @@
+let nombre = prompt ("¿Como es tu nombre?");
+let sexualidad =  prompt ("Ingrese su Sexo: M (Masculino), F (Femenino) o NB (No binario)");
+
+/* while (sexualidad === "M", "m", "F", "f", "NB", "Nb", "nb") {
+switch (sexualidad) {
+    case ("M", "m"):
+        alert ("Bienvenido " + nombre + " a AT sports Training, acontinuación cotizaremos tu entrenamiento");
+        break;
+    case ("F", "m"):
+        alert ("Bienvenida " + nombre + " a AT sports Training, acontinuación cotizaremos tu entrenamiento");
+        break;
+    case ("NB", "Nb", "nb") :
+        alert ("Bienvenide " + nombre + " a AT sports Training, acontinuación cotizaremos tu entrenamiento");
+        break;
+}
+} */
+
+if (sexualidad === "M") {
+    alert ("Bienvenido " + nombre + " a AT sports Training, acontinuación cotizaremos tu entrenamiento");
+} else if (sexualidad === "F") {
+    alert ("Bienvenida " + nombre + " a AT sports Training, acontinuación cotizaremos tu entrenamiento");
+} else {
+    alert ("Bienvenide " + nombre + " a AT sports Training, acontinuación cotizaremos tu entrenamiento");
+}
 
 let numero = parseInt (prompt ("ingrese cantidad de alumnos"));
 
 const precioPersonalizado = 5000;
 const precioDosPersonas = 9000;
 const preciosTresPersonas = 12000;
-const precioPersonaGrupal = 3500;
+const precioGrupal = 3500;
 
 const dosPersonas = 2;
 const tresPersonas = 3;
 const grupal = 4; 
-const numeroPersonas = x;
 
-function calculadora(precioPersonalizado, precioDosPersonas, preciosTresPersonas, precioGrupal, dosPersonas, tresPersonas, grupal, numeroPersonas) {
-    if (numero = 1) {
-        return (precioPersonalizado + "Precio dos días por semana");
-    } else if (numero = 2) {
-        alert (precioDosPersonas / dosPersonas) + "Precio por persona, dos días a la semana";
-    } else if (numero = 3) {
-        alert (preciosTresPersonas / tresPersonas) + "Precio por persona, dos días a la semana";
-    } else (numero => 4) {
-        alert (precioGrupal) + "Precio por persona, dos días a la semana";
-    }
+
+if (numero === 1) {
+    alert ("El entrenamiento personalizado cuesta: " + precioPersonalizado + " al mes, entrenando dos veces a la semana.");
+} else if (numero === 2) {
+    alert ("El entrenamiento en pareja cuesta: " + (precioDosPersonas / dosPersonas) + " por persona, al mes, entrenando dos veces a la semana.");
+} else if (numero === 3) {
+    alert ("El entrenamiento de a tres cuesta: " + (preciosTresPersonas / tresPersonas) + " por persona, al mes, entrenando dos veces a la semana.");
+} else {
+    alert ("El entrenamiento grupal cuesta: " + (precioGrupal) + " por persona, al mes, entrenando dos veces a la semana.");
 }
+
+let consulta = prompt ("¿Le interesa otra cotización? (SI / NO)")
+
+if (consulta === "Si", "SI", "si") {
+    let numero = parseInt (prompt ("ingrese cantidad de alumnos"));
+} else {
+    alert ("Muchas gracias por visitarnos, estamos a tu disposición");
+}
+
 
 
 
