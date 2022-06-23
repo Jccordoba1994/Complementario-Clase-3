32ff94e161549af022f230fa2d9e3a49a414f007
Juan Cruz Cordoba <juanccordoba18@gmail.com>
2022-06-22 23:47:16 -0300

    Entregas

---
+++ Entrega Oblig Clase 4/index.js
@@ -0,0 +1,27 @@
+
+let numero = parseInt (prompt ("ingrese cantidad de alumnos"));
+
+const precioPersonalizado = 5000;
+const precioDosPersonas = 9000;
+const preciosTresPersonas = 12000;
+const precioPersonaGrupal = 3500;
+
+const dosPersonas = 2;
+const tresPersonas = 3;
+const grupal = 4; 
+const numeroPersonas = x;
+
+function calculadora(precioPersonalizado, precioDosPersonas, preciosTresPersonas, precioGrupal, dosPersonas, tresPersonas, grupal, numeroPersonas) {
+    if (numero = 1) {
+        return (precioPersonalizado + "Precio dos días por semana");
+    } else if (numero = 2) {
+        alert (precioDosPersonas / dosPersonas) + "Precio por persona, dos días a la semana";
+    } else if (numero = 3) {
+        alert (preciosTresPersonas / tresPersonas) + "Precio por persona, dos días a la semana";
+    } else (numero => 4) {
+        alert (precioGrupal) + "Precio por persona, dos días a la semana";
+    }
+}
+
+
+
